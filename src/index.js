1fdd477cb2bda2dd737053089ab6aec82eb3cc9b
Jason Miller <jasonmiller@synacor.com>
2016-01-29 11:11:20 -0500

    Fix bootstrap breakage

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { h, render } from 'preact';
 import App from './components/app';
 import './style';
 
-render(<App />, document.body, document.body.firstElementChild);
+render(<App />, document.body);
