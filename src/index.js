a67891a323fe46a8a97d6863b86a54418dce4611
Jason Miller <developit@users.noreply.github.com>
2016-12-09 13:19:05 -0500

    Remove log proxying

--- src/index.js
+++ src/index.js
@@ -12,21 +12,5 @@ function init() {
 init();
 
 if (module.hot) {
-	module.hot.accept('./components/app', () => requestAnimationFrame( () => {
-		flushLogs();
-		init();
-	}) );
-
-	// optional: mute HMR/WDS logs
-	let log = console.log,
-		logs = [];
-	console.log = (t, ...args) => {
-		if (typeof t==='string' && t.match(/^\[(HMR|WDS)\]/)) {
-			if (t.match(/(up to date|err)/i)) logs.push(t.replace(/^.*?\]\s*/m,''), ...args);
-		}
-		else {
-			log.call(console, t, ...args);
-		}
-	};
-	let flushLogs = () => console.log(`%c🚀 ${logs.splice(0,logs.length).join(' ')}`, 'color:#888;');
+	module.hot.accept('./components/app', () => requestAnimationFrame(init) );
 }
