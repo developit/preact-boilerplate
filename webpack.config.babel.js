5bc52c342719405a874b84a77a9c09631501372c
Kuldeep Keshwar <kuldeepkeshwar@KuldeepMac.local>
2016-12-16 03:44:09 +0530

    added sync attr

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,6 +7,7 @@ import ReplacePlugin from 'replace-bundle-webpack-plugin';
 import OfflinePlugin from 'offline-plugin';
 import path from 'path';
 import V8LazyParseWebpackPlugin from 'v8-lazy-parse-webpack-plugin';
+import ScriptExtHtmlWebpackPlugin from "script-ext-html-webpack-plugin";
 const ENV = process.env.NODE_ENV || 'development';
 
 const CSS_MAPS = ENV!=='production';
@@ -118,6 +119,9 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './index.html',
 			minify: { collapseWhitespace: true }
+		}),
+		new ScriptExtHtmlWebpackPlugin({
+			defaultAttribute: "async"
 		}),
 		new CopyWebpackPlugin([
 			{ from: './manifest.json', to: './' },
