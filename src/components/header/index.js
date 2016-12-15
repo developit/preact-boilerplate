2d057488f2cc8408dfafcb50b4d433208f148e7c
Jason Miller <jasonmiller@synacor.com>
2016-12-15 15:43:32 -0500

    shorten title

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>Preact Boilerplate</h1>
+				<h1>Preact App</h1>
 				<nav>
 					<Link href="/">Home</Link>
 					<Link href="/profile">Me</Link>
