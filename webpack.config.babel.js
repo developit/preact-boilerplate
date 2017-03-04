990b32d2ad3251b6f0d5ea3a630a0c7eea0a8190
AdamJo <adamj0101@gmail.com>
2017-03-04 14:14:04 -0800

    config() devServer colors has been deprecated
    if you try to fun the code with colors in it you get an error that
    colors is not in the API scheme
    
    Could not find documentation for this change.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -191,7 +191,6 @@ module.exports = {
 	devServer: {
 		port: process.env.PORT || 8080,
 		host: 'localhost',
-		colors: true,
 		publicPath: '/',
 		contentBase: './src',
 		historyApiFallback: true,
