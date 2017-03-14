2239d5568448db078e7397592c505808de44333a
Klemen Slavič <krof.drakula@gmail.com>
2017-03-14 20:43:53 +0000

    Add explanation of setup

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,7 @@
 import chai from 'chai';
 import assertJsx, { options } from 'preact-jsx-chai';
+// when checking VDOM assertions, don't compare functions,
+// just nodes and attributes
 options.function = false;
+// activate the JSX assertion extension
 chai.use(assertJsx);
