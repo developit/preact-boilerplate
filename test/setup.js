f5ae908e536ee743bab3db7c7b2d56182a913008
Klemen Slavič <krof.drakula@gmail.com>
2017-03-14 22:28:14 +0000

    Workaround for JSDOM/lolex complication

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,4 @@
+import lolex from 'lolex';
 import chai from 'chai';
 import assertJsx, { options } from 'preact-jsx-chai';
 // when checking VDOM assertions, don't compare functions,
@@ -5,3 +6,20 @@ import assertJsx, { options } from 'preact-jsx-chai';
 options.functions = false;
 // activate the JSX assertion extension
 chai.use(assertJsx);
+
+export const createClock = () => {
+	const fns = [
+		'Date', 'setTimeout', 'clearTimeout', 'setInterval', 'clearInterval'
+	];
+	const clock = lolex.install(document.defaultView);
+	// save original JSDOM functions
+	const oldFns = new Map(fns.map(name => [name, global[name]]));
+	// replace with clock
+	fns.forEach(name => global[name] = clock[name]);
+	const uninstall = clock.uninstall;
+	clock.uninstall = () => {
+		uninstall.call(clock);
+		fns.forEach(name => global[name] = oldFns.get(name));
+	};
+	return clock;
+};
