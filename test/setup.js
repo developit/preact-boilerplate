0e6fbeb53258ef8a78c92aa090e0a40befdd5e82
Klemen Slavič <krof.drakula@gmail.com>
2017-03-14 20:57:53 +0000

    Fix options typo

--- test/setup.js
+++ test/setup.js
@@ -2,6 +2,6 @@ import chai from 'chai';
 import assertJsx, { options } from 'preact-jsx-chai';
 // when checking VDOM assertions, don't compare functions,
 // just nodes and attributes
-options.function = false;
+options.functions = false;
 // activate the JSX assertion extension
 chai.use(assertJsx);
