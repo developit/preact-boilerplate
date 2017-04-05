7cdd153e57cd05f436c5a5004305dc2a60184aaa
AdamJo <adamj0101@gmail.com>
2017-04-05 15:59:25 -0700

    config(uglifyJS) add more constraints to uglifyJS

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -139,17 +139,29 @@ module.exports = {
 				comments: false
 			},
 			compress: {
-				conditionals: true,
-				unused: true,
-				comparisons: true,
+				unsafe_comps: true,
+				properties: true,
+				keep_fargs: false,
+				pure_getters: true,
+				collapse_vars: true,
+				unsafe: true,
+				warnings: false,
+				screw_ie8: true,
 				sequences: true,
 				dead_code: true,
+				drop_debugger: true,
+				comparisons: true,
+				conditionals: true,
 				evaluate: true,
+				booleans: true,
+				loops: true,
+				unused: true,
+				hoist_funs: true,
 				if_return: true,
 				join_vars: true,
-				negate_iife: false,
-				warnings: false
-			}
+				cascade: true,
+				drop_console: true
+			},
 		}),
 
 		// strip out babel-helper invariant checks
