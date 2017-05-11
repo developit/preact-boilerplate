883720b21428f6cbee1a9402d99289213ca71889
Jason Miller <jasonmiller@synacor.com>
2017-05-11 11:17:51 -0400

    fix CSS sourceMaps issue /cc @kahlil

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -52,52 +52,54 @@ module.exports = {
 				// Transform our own .(less|css) files with PostCSS and CSS-modules
 				test: /\.(less|css)$/,
 				include: [path.resolve(__dirname, 'src/components')],
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { modules: true, sourceMap: true, importLoaders: 1 }
-            },
-            {
-              loader: `postcss-loader`,
-              options: {
-                plugins: () => {
-                  autoprefixer({ browsers: [ 'last 2 versions' ] });
-                }
-              }
-            },
-            { 
-              loader: 'less-loader',
-              options: { sourceMap: true }
-            }
-          ]
-        })
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							options: { modules: true, sourceMap: CSS_MAPS, importLoaders: 1 }
+						},
+						{
+							loader: `postcss-loader`,
+							options: {
+								sourceMap: CSS_MAPS,
+								plugins: () => {
+									autoprefixer({ browsers: [ 'last 2 versions' ] });
+								}
+							}
+						},
+						{
+							loader: 'less-loader',
+							options: { sourceMap: CSS_MAPS }
+						}
+					]
+				})
 			},
 			{
 				test: /\.(less|css)$/,
 				exclude: [path.resolve(__dirname, 'src/components')],
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { sourceMap: true, importLoaders: 1 }
-            },
-            {
-              loader: `postcss-loader`,
-              options: {
-                plugins: () => {
-                  autoprefixer({ browsers: [ 'last 2 versions' ] });
-                }
-              }
-            },
-            { 
-              loader: 'less-loader',
-              options: { sourceMap: true }
-            }
-          ]
-        })
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							options: { sourceMap: CSS_MAPS, importLoaders: 1 }
+						},
+						{
+							loader: `postcss-loader`,
+							options: {
+								sourceMap: CSS_MAPS,
+								plugins: () => {
+									autoprefixer({ browsers: [ 'last 2 versions' ] });
+								}
+							}
+						},
+						{
+							loader: 'less-loader',
+							options: { sourceMap: CSS_MAPS }
+						}
+					]
+				})
 			},
 			{
 				test: /\.json$/,
@@ -114,11 +116,11 @@ module.exports = {
 		]
 	},
 	plugins: ([
-    new webpack.NoEmitOnErrorsPlugin(),
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true,
-      disable: ENV !== 'production'
+		new webpack.NoEmitOnErrorsPlugin(),
+		new ExtractTextPlugin({
+			filename: 'style.css',
+			allChunks: true,
+			disable: ENV !== 'production'
 		}),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(ENV)
@@ -159,7 +161,7 @@ module.exports = {
 				join_vars: true,
 				cascade: true,
 				drop_console: true
-			},
+			}
 		}),
 
 		// strip out babel-helper invariant checks
