198b8cc28e899d4ef0dd0a9b9303119826a45ec1
antonk52 <antonk52@icloud.com>
2017-09-06 02:09:42 +0300

    turn home component into a stateless functional component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,11 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import style from './style.less';
 
-export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-			</div>
-		);
-	}
-}
+export default () => {
+	return (
+		<div class={style.home}>
+			<h1>Home</h1>
+			<p>This is the Home component.</p>
+		</div>
+	);
+};
